cb6020b04b9e1a68759c88bf3f56afe9b15ea9b8
haru221 <gadget.tf@gmail.com>
2018-05-19 22:07:56 +0900

    Fork/Join フレームワークと実行性能の挑戦問題の解答

--- src/main/scala/ForkJoinMergeSort.scala
+++ src/main/scala/ForkJoinMergeSort.scala
@@ -1,3 +1,5 @@
+import java.util.concurrent.{ForkJoinPool, RecursiveTask}
+
 import scala.util.Random
 
 object ForkJoinMergeSort extends App {
@@ -5,7 +7,31 @@ object ForkJoinMergeSort extends App {
   val randomList = (for (i <- 1 to length) yield Random.nextInt(100)).toList
   println(randomList)
 
+  val pool = new ForkJoinPool()
 
-  val sortedList = ???
+  class MergesortTask(list: List[Int]) extends RecursiveTask[List[Int]] {
+    override def compute(): List[Int] = {
+      val n = list.length / 2
+      if (n == 0) {
+        list
+      } else {
+        val (left, right) = list.splitAt(n)
+        val leftTask = new MergesortTask(left)
+        val rightTask = new MergesortTask(right)
+        leftTask.fork()
+        rightTask.fork()
+        merge(leftTask.join(), rightTask.join())
+      }
+    }
+  }
+
+  private[this] def merge(left: List[Int], right: List[Int]): List[Int] = {
+    if (left.isEmpty) right
+    else if (right.isEmpty) left
+    else if (left.head < right.head) left.head :: merge(left.tail, right)
+    else right.head :: merge(left, right.tail)
+  }
+
+  val sortedList = pool.invoke(new MergesortTask(randomList))
   println(sortedList)
 }
